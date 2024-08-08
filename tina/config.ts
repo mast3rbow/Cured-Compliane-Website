ff5fd65cae3a83652d0880d2438083b14b925ea9
mast3rbow <bryton@curedcompliance.com>
2024-08-08 20:15:21 +1000

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 
   media: {
     tina: {
-      publicFolder: 'public',
+      publicFolder: '/',
       mediaRoot: 'img',
       static: false, //default is false
     },
