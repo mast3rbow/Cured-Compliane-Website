8d1bd5e90f5f05b907757cb6ac157341fbe1237f
mast3rbow <19358000+mast3rbow@users.noreply.github.com>
2026-05-06 15:30:29 +1000

    Use cssnano via PostCSS and update deps
    Replace gulp-cssnano with the cssnano PostCSS plugin in the Gulp CSS task: remove the separate gulp-cssnano step and instead run cssnano inside postcss with a preset that discards comments. Update project dependencies and lockfile accordingly (add cssnano, remove gulp-cssnano, and refresh other dependency entries). Also include a CSS change under static/css/page.css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
-    concatCss = require('gulp-concat-css'),
-    cssnano = require('gulp-cssnano');
+    concatCss = require('gulp-concat-css');
 
 gulp.task('css', function () {
     return gulp.src('./src/css/page.css')
       .pipe(postcss([
         require('tailwindcss'),
         require('autoprefixer'),
+        require('cssnano')({
+          preset: ['default', {
+            discardComments: { removeAll: true }
+          }]
+        })
       ]))
       .pipe(concatCss('page.css'))
-      .pipe(cssnano({
-        reduceIdents: false,
-        discardComments: {removeAll: true}
-      }))
       .pipe(gulp.dest('static/css/'));
   });
